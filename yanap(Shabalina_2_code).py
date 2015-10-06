cb4f6ce5f557c07e6ac260ecc1a7849218755b54
yanaporotikova <lolipop-girl@mil.ru>
2015-10-06 20:02:51 +0300

    Create yanap(Shabalina_2_code).py

---
+++ yanap(Shabalina_2_code).py
@@ -0,0 +1,18 @@
+from lxml import etree
+
+tree = etree.parse('Shabalina_1.xml')
+root = tree.getroot()
+
+for element in tree.iter('patient type'):
+    element.text =u'Bolnoy'
+
+for element in root.iter('hospital'):
+    newattr = element.attrib
+    if newattr:
+        newattr["number"] = "03"
+
+output = etree.tostring(root, pretty_print=True, encoding='UTF-8')
+f1 = open('Shabalina_2.xml', 'w')
+f1.write(output)
+print(output)
+f1.close()
